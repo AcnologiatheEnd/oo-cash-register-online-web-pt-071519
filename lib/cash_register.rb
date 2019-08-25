76f6781ad12d79fde14d056b9af0a4e3c9fabf98
Winston  Chung <18.chung.winston@ctkprep.org>
2019-08-25 01:11:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
 
+
+class CashRegister
+ attr_accessor :total, :discount, :items
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity=1)
+    @title = title
+    @price = price
+    @quantity = quantity
+    item_total = price*quantity
+    @total = @total+=item_total
+    
+    count = 0
+    while count < quantity
+      @items << @title
+      count += 1
+    end
+  end
+  
+  def apply_discount
+    if @discount == 0
+      return "There is no discount to apply."
+    else
+      percent = @discount.to_f/100
+      dis = total * percent
+      @total -= dis
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+  
+  def void_last_transaction
+    @total -= @price
+  end
+end
+
